eda47d49309553d8a6f9bbf94ca60e952ac89450
marc-inn <0.660162@gmail.com>
2015-11-22 21:33:48 +0100

    Add implementation of chars

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -30,7 +30,10 @@ object Huffman {
     case Leaf(_, weight) => weight
   }
 
-  def chars(tree: CodeTree): List[Char] = ??? // tree match ...
+  def chars(tree: CodeTree): List[Char] = tree match {
+    case Fork(_, _, chars, _) => chars
+    case Leaf(char, _) => List(char)
+  }
 
   def makeCodeTree(left: CodeTree, right: CodeTree) =
     Fork(left, right, chars(left) ::: chars(right), weight(left) + weight(right))
