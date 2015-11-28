9ef9b131668ff92a8039260ce85804682e9ea890
marc-inn <0.660162@gmail.com>
2015-11-29 00:36:29 +0100

    Add implementation of convert

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -249,7 +249,12 @@ object Huffman {
     * a valid code tree that can be represented as a code table. Using the code tables of the
     * sub-trees, think of how to build the code table for the entire tree.
     */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    tree match {
+      case Leaf(ch, w) => List((ch, List()))
+      case Fork(l, r, lCh, w) => mergeCodeTables(convert(l), convert(r))
+    }
+  }
 
   /**
     * This function takes two code tables and merges them into one. Depending on how you
